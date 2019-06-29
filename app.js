7e4fe5d619a6b8954f3725da76b4df3f9ce3b4b2
rogerwschmidt <rogerschmidt@gmail.com>
2019-06-28 21:01:02 -0700

    added docker

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express()
 // Mongoose setup //////////////
 
 // connect to database books
-const db = mongoose.connect('mongodb://localhost/books', { useNewUrlParser: true } )
+const db = mongoose.connect('mongodb://db/books', { useNewUrlParser: true } )
 
 // create schema for your data
 const bookSchema = new mongoose.Schema({
@@ -31,18 +31,36 @@ app.use(bodyParser.json())
 
 app.post('/books', (req, res, next) => {
   // TODO: add create code here 
+  const { title, author } = req.body
+
+  Book.create({ title, author }, (err, book) => {
+    if(err) return res.status(500).send(err)
+    res.send(book)
+  })
 })
 
 app.get('/books', (req, res, next) => {
-  // TODO: add get all code here 
+  // TODO: add get all code here
+  Book.find({}, function(err, books){
+    if(err) return res.status(500).send(err)
+    res.send(books)
+  })
 })
 
-app.get('/books/:id', (req, res, next) => {
-  // TODO: add get ont code here 
+app.get('/books/:bookId', (req, res, next) => {
+  // TODO: add get ont code here
+  Book.findById(req.params.bookId, (err, book) => {
+    if(err) return res.status(500).send(err)
+    res.send(book)
+  })
 })
 
 app.delete('/books/:id', (req, res, next) => {
   // TODO: add delete one code here 
+  Book.deleteOne({ _id: req.params.id}, (err, book) => {
+    if(err) return res.status(500).send(err)
+    res.send(book)
+  })
 })
 
 app.patch('/books/:id', (req, res, next) => {
